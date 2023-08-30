cd03070b76ed19d9acd378f9cc42e63ca86b281e
Daniel John <dj71286@gmail.com>
2023-08-30 22:00:54 +0100

    Fixed annotations

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """ An implementation of Personal User Data """
-
 import re
+from typing import List
 
 
-def filter_datum(fields: list, redaction: str, message: str, separator: str):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """ A function that returns the log message obfuscated """
     pattern = rf'({"|".join(fields)})=[^{separator}]+'
     return re.sub(pattern, f'\\1={redaction}', message)
