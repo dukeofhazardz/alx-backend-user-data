113a0def795e48b45c5cf4d44d1d1db055b5a8a4
Daniel John <dj71286@gmail.com>
2023-09-01 01:38:30 +0100

    Added edge cases

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,17 +7,21 @@ from typing import Literal
 def hash_password(password: str) -> bytes:
     """ A function that returns a salted, hashed password,
         which is a byte string. """
-    password_bytes: bytes = password.encode('utf-8')
-    hashed_pw: bytes = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
-    return hashed_pw
+    if password:
+        password_bytes: bytes = password.encode('utf-8')
+        hashed_pw: bytes = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+        return hashed_pw
+    return None
 
 
 def is_valid(hashed_password: bytes,
              password: str) -> Literal[True] | Literal[False]:
     """ A function that validates that the provided password
         matches the hashed password. """
-    password_bytes: bytes = password.encode('utf-8')
-    pw_status = bcrypt.checkpw(password_bytes, hashed_password)
-    if pw_status:
-        return True
+    if hashed_password and password:
+        password_bytes: bytes = password.encode('utf-8')
+        pw_status = bcrypt.checkpw(password_bytes, hashed_password)
+        if pw_status:
+            return True
+        return False
     return False
