8acb0b475cd85d17773cef51c24540257fb80e35
Daniel John <dj71286@gmail.com>
2023-09-01 01:49:04 +0100

    Refactored password encryption code

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,21 +7,13 @@ from typing import Literal
 def hash_password(password: str) -> bytes:
     """ A function that returns a salted, hashed password,
         which is a byte string. """
-    if password:
-        password_bytes: bytes = password.encode('utf-8')
-        hashed_pw: bytes = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
-        return hashed_pw
-    return None
+    hashed_pw: bytes = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_pw
 
 
 def is_valid(hashed_password: bytes,
              password: str) -> Literal[True] | Literal[False]:
     """ A function that validates that the provided password
         matches the hashed password. """
-    if hashed_password and password:
-        password_bytes: bytes = password.encode('utf-8')
-        pw_status = bcrypt.checkpw(password_bytes, hashed_password)
-        if pw_status:
-            return True
-        return False
-    return False
+    pw_status = bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    return True if pw_status else False
