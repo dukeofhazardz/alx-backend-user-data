7b8ccc9800444c37d3c817a24e2c32e240461d39
Daniel John <dj71286@gmail.com>
2023-09-05 20:44:39 +0100

    Defined which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,8 +8,17 @@ from typing import List, TypeVar
 class Auth:
     """ The Auth Class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ A public method that returns returns False - path
-            and excluded_paths """
+        """ A public method that returns True if the path is not in the
+            list of strings excluded_paths """
+        if path == None or excluded_paths == None or excluded_paths == []:
+            return True
+        for excluded_path in excluded_paths:
+            if not path.endswith('/'):
+                path += '/'
+            if not excluded_path.endswith('/'):
+                excluded_path += '/'
+        if path not in excluded_paths:
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
