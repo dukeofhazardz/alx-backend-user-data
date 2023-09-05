5db44a7ae8f7538210f7c80c8e09d4daa9707034
Daniel John <dj71286@gmail.com>
2023-09-05 22:35:47 +0100

    Added Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -25,12 +25,31 @@ class BasicAuth(Auth):
                                            str) -> str:
         """ A method that returns the decoded value of a
             Base64 string base64_authorization_header """
-        if base64_authorization_header is None or not isinstance(
-            base64_authorization_header, str):
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
             return None
         try:
             base64_decoded = base64.b64decode(base64_authorization_header)
         except base64.binascii.Error:
             return None
-        
+
         return base64_decoded.decode('utf-8')
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """ A method that returns the user email and password from the
+            Base64 decoded value """
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        t: tuple = decoded_base64_authorization_header.split(':')
+        return t[0], t[1]
