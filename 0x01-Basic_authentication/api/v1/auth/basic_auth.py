b57fb88d0fff05a65d190b104b3c56e731f763d9
Daniel John <dj71286@gmail.com>
2023-09-07 00:21:05 +0100

    Added Basic - Allow password with ':'

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,7 +54,7 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
 
-        t: tuple = decoded_base64_authorization_header.split(':')
+        t: tuple = decoded_base64_authorization_header.split(':', 1)
         return t[0], t[1]
 
     def user_object_from_credentials(self,
